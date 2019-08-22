1af717a2b17192bf55e878169ac617d5614eb590
yourawizardharry <53281501+yourawizardharry@users.noreply.github.com>
2019-08-22 13:11:37 +1200

    Add files via upload

---
+++ Parallax.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    GameObject backGround1, backGround2;
+    RectTransform rt;
+    float backGround1LocY, backGround2LocY;
+    float cameraLocY;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (backGround1 == null || backGround2 == null)
+        {
+            backGround1 = GameObject.Find("underwater");
+            backGround2 = GameObject.Find("underwater2");
+        }
+        else if (backGround1 != null && backGround2 != null)
+        {
+            backGround1LocY = backGround1.transform.position.y - (backGround1.GetComponent<SpriteRenderer>().bounds.size.y / 2f);
+            backGround2LocY = backGround2.transform.position.y - (backGround2.GetComponent<SpriteRenderer>().bounds.size.y / 2f);
+            cameraLocY = GetComponent<Camera>().transform.position.y - GetComponent<Camera>().orthographicSize;
+
+            if(cameraLocY < backGround1LocY + -1*backGround1.GetComponent<SpriteRenderer>().bounds.size.y)
+            {
+                Vector3 temp1 = backGround2.transform.position;
+                temp1.y += (-1 * backGround2.GetComponent<SpriteRenderer>().bounds.size.y);
+                backGround1.transform.position = temp1;
+            }
+            else if (cameraLocY < backGround2LocY + -1 * backGround2.GetComponent<SpriteRenderer>().bounds.size.y)
+            {
+                Vector3 temp2 = backGround1.transform.position;
+                temp2.y += (-1 * backGround1.GetComponent<SpriteRenderer>().bounds.size.y);
+                backGround2.transform.position = temp2;
+            }
+        }
+    }
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float playerMovementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMovementSpeed = 0.2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vect = transform.position;
+        vect.y -= playerMovementSpeed;
+        transform.position = vect;
+        //GameObject.FindWithTag("MainCamera").transform.position = new Vector3 (GameObject.FindWithTag("MainCamera").transform.position.x, transform.position.y, -10);
+        Camera.main.transform.position = new Vector3(GameObject.FindWithTag("MainCamera").transform.position.x, transform.position.y, -10);
+    }
+}
