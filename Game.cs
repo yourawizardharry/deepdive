7ee756045880ca7b2eae114eb1fb1ce5db90f10a
yourawizardharry <53281501+yourawizardharry@users.noreply.github.com>
2019-09-06 09:31:01 +1200

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+
+{
+	public static bool gameRunning = true;
+
+	public static void stopGame() 
+	{
+		gameRunning = false;
+	}
+
+	public static void startGame()
+	{
+		gameRunning = true;
+	}
+
+}
