e9d449ad8dfcacfd540e95abe8e9e3363313c9cd
yourawizardharry <53281501+yourawizardharry@users.noreply.github.com>
2019-09-06 09:31:14 +1200

    Add files via upload

--- Parallax.cs
+++ Parallax.cs
@@ -9,33 +9,54 @@ public class Parallax : MonoBehaviour
     float backGround1LocY, backGround2LocY;
     float cameraLocY;
 
+    void Start()
+    {
+        getBackgrounds();
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        if (backGround1 == null || backGround2 == null)
-        {
-            backGround1 = GameObject.Find("underwater");
-            backGround2 = GameObject.Find("underwater2");
-        }
-        else if (backGround1 != null && backGround2 != null)
+		if(Game.gameRunning) {
+			if (backGround1 == null || backGround2 == null)
+			{
+			    getBackgrounds();
+			}
+			else backGroundUpdate();
+		}
+    }
+
+
+    public void backGroundUpdate()
+    {
+        if (backGround1 != null && backGround2 != null)
         {
             backGround1LocY = backGround1.transform.position.y - (backGround1.GetComponent<SpriteRenderer>().bounds.size.y / 2f);
             backGround2LocY = backGround2.transform.position.y - (backGround2.GetComponent<SpriteRenderer>().bounds.size.y / 2f);
             cameraLocY = GetComponent<Camera>().transform.position.y - GetComponent<Camera>().orthographicSize;
 
-            if(cameraLocY < backGround1LocY + -1*backGround1.GetComponent<SpriteRenderer>().bounds.size.y)
+            if (cameraLocY < backGround1LocY + -1 * backGround1.GetComponent<SpriteRenderer>().bounds.size.y)
             {
-                Vector3 temp1 = backGround2.transform.position;
-                temp1.y += (-1 * backGround2.GetComponent<SpriteRenderer>().bounds.size.y);
-                backGround1.transform.position = temp1;
+                moveBackground(backGround1, backGround2);
             }
             else if (cameraLocY < backGround2LocY + -1 * backGround2.GetComponent<SpriteRenderer>().bounds.size.y)
             {
-                Vector3 temp2 = backGround1.transform.position;
-                temp2.y += (-1 * backGround1.GetComponent<SpriteRenderer>().bounds.size.y);
-                backGround2.transform.position = temp2;
+                moveBackground(backGround2, backGround1);
             }
         }
+    }
+
+    public void getBackgrounds()
+    {
+        backGround1 = GameObject.Find("underwater");
+        backGround2 = GameObject.Find("underwater2");
+    }
+
+    public void moveBackground(GameObject backgroundToMove, GameObject backGroundCompare)
+    {
+        Vector3 temp = backGroundCompare.transform.position;
+        temp.y += (-1 * backGroundCompare.GetComponent<SpriteRenderer>().bounds.size.y);
+        backgroundToMove.transform.position = temp;
     }
 }
