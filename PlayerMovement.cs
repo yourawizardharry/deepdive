3de80fe172b5d4845fa9f461d51aa6d30223a571
yourawizardharry <53281501+yourawizardharry@users.noreply.github.com>
2019-09-06 09:31:31 +1200

    Add files via upload

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -5,20 +5,44 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float playerMovementSpeed;
+	private float playerPositioningOffset;
 
     // Start is called before the first frame update
     void Start()
     {
         playerMovementSpeed = 0.2f;
+		playerPositioningOffset = 4f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 vect = transform.position;
-        vect.y -= playerMovementSpeed;
-        transform.position = vect;
-        //GameObject.FindWithTag("MainCamera").transform.position = new Vector3 (GameObject.FindWithTag("MainCamera").transform.position.x, transform.position.y, -10);
-        Camera.main.transform.position = new Vector3(GameObject.FindWithTag("MainCamera").transform.position.x, transform.position.y, -10);
+        if(Game.gameRunning) movePlayer();
     }
+
+	private void movePlayer() 
+	{
+		Vector3 newPostition = getNewPosition();
+        transform.position = newPostition;
+		updateCameraPosition(newPostition.y);
+	}
+
+	private void updateCameraPosition(float Ypos) 
+	{
+		Camera.main.transform.position = new Vector3(GameObject.FindWithTag("MainCamera").transform.position.x, Ypos-playerPositioningOffset, -10);
+	}
+
+	private Vector3 getNewPosition() 
+	{
+		Vector3 vect = transform.position;
+        vect.y -= playerMovementSpeed;
+		return vect;
+	}
+
+	void setPlayerSpeed(float speed) 
+	{
+		if(speed > 0 && speed <=1) {
+			playerMovementSpeed = speed;
+		}
+	}
 }
