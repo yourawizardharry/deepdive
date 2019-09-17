e92520d268aeed1b206573f802b1cc2a328be3ac
Vaandite <53281594+Vaandite@users.noreply.github.com>
2019-09-18 00:20:42 +1200

    Add files via upload

---
+++ Score.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Transform player;
+    public Text scoreText;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        float depth = player.position.y * (-1)/10;
+        scoreText.text = depth.ToString("0");
+    }
+}
