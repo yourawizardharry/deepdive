440b405ca4d883e087e1d03e849ce2586ccdb39c
Vaandite <53281594+Vaandite@users.noreply.github.com>
2019-09-18 02:00:35 +1200

    Add files via upload

---
+++ O2.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class O2 : MonoBehaviour
+{
+    public Transform player;
+    public Text scoreText;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        float oxygenLeft = 100 - player.position.y * (-1) / 80; ;
+        if(oxygenLeft>0)
+        {
+            scoreText.text = "Oxygen "+oxygenLeft.ToString("0")+"%";
+        }
+    }
+}
