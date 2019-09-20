4cf50cd81fc59505e3ce3fc5c0fc3f4d1d05b42d
Vaandite <53281594+Vaandite@users.noreply.github.com>
2019-09-20 12:22:40 +1200

    Add files via upload

---
+++ PlayerControls.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+	private float sensitivity = 15;
+	Camera camera;
+	float boundMin, boundMax;
+	public Sprite leftImage, rightImage, downImage;
+    bool running = true;
+    public uiManager ui;
+
+	void Start() {
+		if(camera!= null) getBounds();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(running  && camera!=null) {
+		float acceleration = getAcceleration();
+		if(acceleration > 1.2f / sensitivity) setImage(rightImage);
+		else if(acceleration < -1.2f / sensitivity) setImage(leftImage);
+		else if(acceleration > -0.6f / sensitivity && acceleration < 0.6f / sensitivity) setImage(downImage);
+		updatePosition(getPosition(acceleration));
+		}
+    }
+
+	void setImage(Sprite image) {
+	if(image!=null) GetComponent<SpriteRenderer>().sprite = image;
+	else Debug.Log("imagenull");
+	}
+
+	public void addCamera(Camera camera)
+	{
+		this.camera = camera;
+	}
+
+    public void isRunning(bool running)
+    {
+        this.running = running;
+    }
+
+
+	bool isWithinBounds(float x) {
+		if(x+transform.position.x < boundMax && x+transform.position.x > boundMin) {
+			return true;
+		}
+		else return false;
+		
+	}
+
+	void getBounds() {
+		boundMax = (camera.aspect * camera.orthographicSize) - 1;
+		boundMin = boundMax*-1;
+	}
+
+	float getAcceleration() 
+	{
+		return Input.acceleration.x;
+	}
+
+	float getPosition(float acceleration) {
+		return acceleration*sensitivity*Time.deltaTime;
+	}
+
+	public void updatePosition(float acceleration) {
+		if(isWithinBounds(acceleration)) transform.Translate(acceleration, 0, 0);
+	}
+
+	bool setSensitivity(float sensitivity) {
+		if(sensitivity < 20 && sensitivity > 5) {
+			this.sensitivity = sensitivity;
+			return true;
+		}
+		else return false;
+	}
+
+}
---
+++ Score.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public GameObject player;
+    public Text scoreText;
+
+    private void Start()
+    {
+        player = Instantiate(player);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float depth = player.transform.position.y * (-1) / 10 - 4;
+        scoreText.text = "Score: " + depth.ToString("0");
+    }
+
+    public void addPlayer(GameObject player)
+    {
+        //this.player = GameObject.Find("player").transform;
+        this.player = player;
+    }
+}
