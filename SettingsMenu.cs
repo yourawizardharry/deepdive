0820ebc60c2b789215e7ef38c6831f56a94a37ce
yourawizardharry <53281501+yourawizardharry@users.noreply.github.com>
2019-09-30 21:48:11 +1300

    Add files via upload

---
+++ SettingsMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+ using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+	Slider slider;
+	
+	void Start() {
+		slider = GameObject.Find("Slider").GetComponent<Slider>();
+		slider.onValueChanged.AddListener(delegate {callBackSlider(slider.value);});
+	}
+
+	 public void callBackSlider(float newValue)
+ {
+	Debug.Log(newValue);
+     PlayerControls.setSensitivity(newValue);
+ }
+
+ public void returnToMenu() {
+	SceneManager.LoadScene(0);
+ }
+
+}
