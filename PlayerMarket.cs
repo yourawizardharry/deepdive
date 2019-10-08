909daaf75bac434da7739b41e42da01293035024
yourawizardharry <53281501+yourawizardharry@users.noreply.github.com>
2019-10-08 13:34:25 +1300

    Add files via upload

--- PlayerMarket.cs
+++ PlayerMarket.cs
@@ -5,17 +5,34 @@ using UnityEngine.UI;
 
 public class PlayerMarket : MonoBehaviour
 {
+    private string saveKey = "playerImg";
     public Sprite diverSprite;
     public Sprite newPlayer2;
     public Sprite newPlayer3;
+    public Sprite curSprite;
     private ArrayList playerList;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerList = new ArrayList();
         playerList.Add(diverSprite);
         playerList.Add(newPlayer2);
         playerList.Add(newPlayer3);
+        if(PlayerPrefs.HasKey(saveKey))
+        {
+            curSprite = (Sprite)playerList.ToArray()[PlayerPrefs.GetInt(saveKey)];
+        }
+        else
+        {
+            curSprite = diverSprite;
+        }
+        setPlayerImage(curSprite);
+    }
+
+    public void saveCurrentImageSelection()
+    {
+        if (playerList.Contains(curSprite)) PlayerPrefs.SetInt(saveKey, playerList.IndexOf(curSprite));
     }
    
     public void setPlayerImage(Sprite newPlayerImage) 
@@ -24,6 +41,7 @@ public class PlayerMarket : MonoBehaviour
         PlayerControls playerControlsScript = diverObject.GetComponent<PlayerControls>();
         diverObject.GetComponent<SpriteRenderer>().sprite = newPlayerImage;
         playerControlsScript.setPlayerImage(null, null, newPlayerImage);
+        curSprite = newPlayerImage;
     }
 
     public ArrayList getPlayerSprites()
