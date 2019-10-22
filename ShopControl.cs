28fe7e7bdea61d05fced4e375953787815e56144
Seagull1239 <53281700+Seagull1239@users.noreply.github.com>
2019-10-22 20:21:18 +1300

    Create ShopControl.cs

---
+++ ShopControl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShopControl : MonoBehaviour
+{
+
+
+
+
+
+    public void mainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
